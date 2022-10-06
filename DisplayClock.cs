9bc7bb037dc0dfe01bf6ef54e050f58586739876
Fannabykiki <namplhe153292@fpt.edu.vn>
2022-10-06 23:57:03 +0700

    Initial commit

---
+++ DisplayClock.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Rookies.Events
+{
+    public class DisplayClock
+    {
+        public void Subcribe(Clock clock)
+        {
+            clock.clockTick += new Clock.clockTickHandler(ShowClock);
+        }
+        public void ShowClock(object clock, ClockEventArgs clockEventArgs)
+        {
+            System.Console.WriteLine($"{clockEventArgs.hour} : {clockEventArgs.minute} : {clockEventArgs.second}  ");
+        }
+    }
+}
